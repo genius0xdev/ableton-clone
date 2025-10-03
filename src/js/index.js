c7b0d1456f5b419955080fdf331bd8816f95e041
dev0xgenius <dev0xgenius@gmail.com>
2025-10-03 04:17:11 +0100

    Update styling

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,15 @@ let logo = document.querySelector(".logo");
 toggler.addEventListener("click", handleToggler);
 
 function handleToggler(evt) {
-  animateToggle([toggler, menuContent, logo]);
+    animateToggle([toggler, menuContent, logo]);
 }
 
-function animateToggle(activeElements, classesToadd=[]) {
-  classesToadd.push("active");
-  
-  activeElements.forEach(element => 
-    !element.classList.contains("active") ?
-      classesToadd.forEach(item => element.classList.add(item)) :
-      classesToadd.forEach(item => element.classList.remove(item))
-  );
+function animateToggle(activeElements, classesToadd = []) {
+    classesToadd.push("active");
+
+    activeElements.forEach((element) =>
+        !element.classList.contains("active")
+            ? classesToadd.forEach((item) => element.classList.add(item))
+            : classesToadd.forEach((item) => element.classList.remove(item))
+    );
 }
